a04903becbe5d2ac1482f21861a0d35f3e50e197
Noah Beach <noahbeach@Noahs-MacBook-Air.local>
2026-03-08 19:51:12 -0500

    Complete Django Girls extension section

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.text
